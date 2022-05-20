781e57b2232362dd9ead2da042be04d76b41471e
VanAnhPH13707 <79831956+VanAnhPH13707@users.noreply.github.com>
2022-05-20 22:39:39 +0700

    update

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -13,52 +13,68 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
   inputValues = {
+    id: 0,
     name: '',
     age: 0,
-    email:'',
+    email: '',
     sdt: 0
   };
   users = [
     {
-      id:1,
-      name:'anhntv',
+      id: 1,
+      name: 'anhntv',
       age: 20,
-      email:'anhntvph13707@fpt.edu.vn',
+      email: 'anhntvph13707@fpt.edu.vn',
       sdt: 975703356
-      
+
+    },
+    {
+      id: 2,
+      name: 'Vân Anh',
+      age: 20,
+      email: 'vananh@gmail.com',
+      sdt: 975703356
+
     }
   ];
-  onSubmit(userForm:NgForm){
+  onSubmit(userForm: NgForm) {
     console.log(userForm.value);
-    
+
     // console.log(formData);
     //0.Tìm id lớn nhất đang có để + 1
     const newUserIds = this.users
-    .map(users => users.id) // lấy ra mảng mới chỉ có id
-    .sort((a:number, b:number) => b-a); // sort các id đang có
+      .map(users => users.id) // lấy ra mảng mới chỉ có id
+      .sort((a: number, b: number) => b - a); // sort các id đang có
     const maxId = newUserIds[0]
     //1. Push dữ liệu mới nhận từ form vào mảng
-    if (userForm.value.id === 0){
+    if (userForm.value.id === 0) {
       this.users.push({
         ...userForm.value,
-        id: maxId +1
+        id: maxId + 1
       });
-    }else{
-
+    } else {
+      this.inputValues = ({
+        ...userForm.value,
+        id: this.inputValues.id
+      })
     }
-    
+
     //2. Cập nhật giá trị của inputValues về default
-    userForm.resetForm ({
-      name:'',
+    userForm.resetForm({
+      id: 0,
+      name: '',
       age: 0,
-      email:'',
+      email: '',
       sdt: 0
     });
   }
-  onRemove(id:number){
-    this.users = this.users.filter(item => item.id !== id)
+  onRemove(id: number) {
+    const confirm = window.confirm("Bạn chắc chắn muốn xóa ?")
+    if (confirm) {
+      this.users = this.users.filter(item => item.id !== id)
+    }
   }
-  onEdit(user: any){
-    this.inputValues = user;
+  onEdit(user: any, id: number) {
+    this.inputValues = user.find((item: any) => item.id === id);
   }
 }
