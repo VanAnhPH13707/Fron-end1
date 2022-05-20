d3a8ea2307b54e26195eb7250933e245bb5438c1
VanAnhPH13707 <79831956+VanAnhPH13707@users.noreply.github.com>
2022-05-20 19:45:49 +0700

    update

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -11,8 +12,53 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onSubmit(formData: {name:string, age: string}){
-    console.log(formData);
+  inputValues = {
+    name: '',
+    age: 0,
+    email:'',
+    sdt: 0
+  };
+  users = [
+    {
+      id:1,
+      name:'anhntv',
+      age: 20,
+      email:'anhntvph13707@fpt.edu.vn',
+      sdt: 975703356
+      
+    }
+  ];
+  onSubmit(userForm:NgForm){
+    console.log(userForm.value);
     
+    // console.log(formData);
+    //0.Tìm id lớn nhất đang có để + 1
+    const newUserIds = this.users
+    .map(users => users.id) // lấy ra mảng mới chỉ có id
+    .sort((a:number, b:number) => b-a); // sort các id đang có
+    const maxId = newUserIds[0]
+    //1. Push dữ liệu mới nhận từ form vào mảng
+    if (userForm.value.id === 0){
+      this.users.push({
+        ...userForm.value,
+        id: maxId +1
+      });
+    }else{
+
+    }
+    
+    //2. Cập nhật giá trị của inputValues về default
+    userForm.resetForm ({
+      name:'',
+      age: 0,
+      email:'',
+      sdt: 0
+    });
+  }
+  onRemove(id:number){
+    this.users = this.users.filter(item => item.id !== id)
+  }
+  onEdit(user: any){
+    this.inputValues = user;
   }
 }
